deb81113adadcac0990623d6f659ee6d3fdd7ec6
Fadni <fadnihdr@gmail.com>
2016-11-29 13:27:50 +0800

    billing completed

--- src/Billing.java
+++ src/Billing.java
@@ -1,39 +1,51 @@
-/**
- * Created by fadni on 29/11/2016.
- */
 public class Billing {
-    double totalDue;
-    public void main(String[] args){
+    public static void main(String[] args){
+        double coupon = 0;
+        double price = 12;
+        double quantity = 0;
+        if (price>0){
+            if (quantity>0) {
+                if (coupon > 0) {
+                    computeBill(price,quantity,coupon);
+                }
+                else {
+                    computeBill(price, quantity);
+                }
+            }
+            else {
+                computeBill(price);
+            }
+        }
 
-        computeBill(2.5,4.5,22);
-        System.out.println(totalDue);
     }
-    public double computeBill(double price, double quantity, double coupon){
+
+    public static double computeBill(double price, double quantity, double coupon){
         double totalDue;
         double totalPrice;
         double tax = 0.08;
-        if (price>0){
-            if (quantity>0){
-                if (coupon>0){
-                    totalPrice = ((price * quantity) - coupon);
-                    totalDue = (totalPrice + (tax * totalPrice));
-                    return totalDue;
-                }
-                else {
-                    totalPrice = ((price * quantity));
-                    totalDue = totalPrice + ((tax * totalPrice));
-                    return totalDue;
-                }
-            }
-        }
-        else{
-            totalPrice = price;
-            totalDue = (totalPrice + (tax * totalPrice));
-            return totalDue;
-        }
-        return -1;
+        totalPrice = ((price * quantity) - coupon);
+        totalDue = (totalPrice + (tax * totalPrice));
+        System.out.println(totalDue);
+        return totalDue;
+    }
 
+    public static double computeBill(double price, double quantity){
+        double totalDue;
+        double totalPrice;
+        double tax = 0.08;
+        totalPrice = ((price * quantity));
+        totalDue = totalPrice + ((tax * totalPrice));
+        System.out.println(totalDue);
+        return totalDue;
+    }
 
-
+    public static double computeBill(double price){
+        double totalDue;
+        double totalPrice;
+        double tax = 0.08;
+        totalPrice = price;
+        totalDue = (totalPrice + (tax * totalPrice));
+        System.out.println(totalDue);
+        return totalDue;
     }
 }
